12b10701fef62ff9953a8284d1bc3520ce48e095
Kris <kris@docnetwork.org>
2019-05-25 18:20:05 -0400

    adds check for users location and sets marker

--- client/src/App.js
+++ client/src/App.js
@@ -7,32 +7,50 @@ import './App.css';
 var myIcon = L.icon({
   iconUrl: 'https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon-2x.png',
   iconSize: [25, 41],
-  iconAnchor: [22, 94],
-  popupAnchor: [-10, -85]
+  iconAnchor: [12.5, 41],
+  popupAnchor: [0, -41]
 });
 
 class App extends Component {
   state = {
+    location: {
     lat: 51.505,
     lng: -0.09,
-    zoom: 13,
+    },
+    haveUsersLocation: false,
+    zoom: 2,
   }
+componentDidMount() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    this.setState({
+      location: {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      },
+      haveUsersLocation: true,
+      zoom: 13
+    });
+  });
+}
 
   render() {
-  const position = [this.state.lat, this.state.lng]
+  const position = [this.state.location.lat, this.state.location.lng]
   return (
     <Map className="map" center={position} zoom={this.state.zoom}>
     <TileLayer
       attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
+    {
+     this.state.haveUsersLocation ?
     <Marker 
       position={position}
       icon={myIcon}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
-    </Marker>
+    </Marker> : ''
+    }
   </Map>
   );
 }
