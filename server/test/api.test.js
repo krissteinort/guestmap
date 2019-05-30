a19b85af81e0ddd5b43431dbaf820e2ce4e5f08f
Kris <kris@docnetwork.org>
2019-05-30 12:16:13 -0400

    sets up a test for POST message route

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -14,12 +14,30 @@ describe('GET /api/v1', () => {
   });
 });
 
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', function(done) {
+describe('POST /api/v1/messages', () => {
+  it('responds with inserted message', (done) => {
+    const requestObj = {
+      name: 'CJ',
+      message: 'This app is so cool!',
+      latitude: -90,
+      longitude: 180
+    };
+
+    const responseObj = {
+      ...requestObj,
+      _id: '5b57d127923211248855977c',
+      date: '2018-07-25T01:23:51.029Z'
+    };
+
     request(app)
-      .get('/api/v1/emojis')
+      .post('/api/v1/messages')
+      .send(requestObj)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, ['😀', '😳', '🙄'], done);
+      .expect(res => {
+        res.body._id = '5b57d127923211248855977c';
+        res.body.date = '2018-07-25T01:23:51.029Z';
+      })
+      .expect(200, responseObj, done);
   });
 });
